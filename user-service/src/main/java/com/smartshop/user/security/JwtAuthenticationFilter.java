486f310831821851e2553c9897188de3739ed8f4
Jagannath Adhikary <jagannath.adhikary@sap.com>
2026-05-30 09:55:27 +0530

    feat: unload authentication and register to eureka

--- user-service/src/main/java/com/smartshop/user/security/JwtAuthenticationFilter.java
+++ user-service/src/main/java/com/smartshop/user/security/JwtAuthenticationFilter.java
@@ -1,9 +1,5 @@
 package com.smartshop.user.security;
 
-import com.smartshop.user.service.JwtService;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.MalformedJwtException;
 import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -16,7 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -25,47 +20,34 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-  private final JwtService jwtService;
   private final UserDetailsService userDetailsService;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    String jwt = fetchJwtToken(request);
-    if(!StringUtils.isEmpty(jwt)) {
-      try {
-        String username = jwtService.extractUsername(jwt);
-        UserDetails user = userDetailsService.loadUserByUsername(username);
-        if (jwtService.validateToken(jwt, user)) {
-          UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user, null,
-              user.getAuthorities());
-          authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-          SecurityContextHolder.getContext().setAuthentication(authToken);
-        }
-      } catch(ExpiredJwtException e) {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-        response.getWriter().write("{\"error\":\"JWT is expired\"}");
-        return;
-      } catch(JwtException e) {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-        response.getWriter().write("{\"error\":\"Invalid JWT\"}");
+    if(!request.getServletPath().startsWith("/api/auth/")) {
+      String username = request.getHeader("X-User-Id");
+      if (StringUtils.isEmpty(username)) {
+        unauthorized(response, "{\"error\":\"Unauthorized Source\"}");
         return;
       }
-      catch(UsernameNotFoundException e) {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-        response.getWriter().write("{\"error\":\"Token holder is not a valid user\"}");
+      try {
+        UserDetails user = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user, null,
+                user.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+      } catch (UsernameNotFoundException e) {
+        unauthorized(response, "{\"error\":\"Token holder is not a valid user\"}");
         return;
       }
     }
     filterChain.doFilter(request, response);
   }
 
-  private String fetchJwtToken(HttpServletRequest request) {
-    String authHeader = request.getHeader("Authorization");
-    if(authHeader == null || !authHeader.startsWith("Bearer ")) return null;
-    return authHeader.substring(7);
+  private void unauthorized(HttpServletResponse response, String message) throws IOException {
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.setContentType("application/json");
+    response.getWriter().write(message);
   }
 }
